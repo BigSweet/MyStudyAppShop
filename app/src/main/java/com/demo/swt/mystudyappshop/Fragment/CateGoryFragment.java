f3781566ddd6296fe6e0e8ca791aedd0b440cfc0
sunwentao <sunwentao@imcoming.cn>
2017-01-04 17:19:21 +0800

    看了博客之后小试牛刀，自定义了一个简单的view，然后开始搞recyclerview

--- app/src/main/java/com/demo/swt/mystudyappshop/Fragment/CateGoryFragment.java
+++ app/src/main/java/com/demo/swt/mystudyappshop/Fragment/CateGoryFragment.java
@@ -1,5 +1,6 @@
 package com.demo.swt.mystudyappshop.Fragment;
 
+import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.demo.swt.mystudyappshop.R;
+import com.demo.swt.mystudyappshop.SelectCityActivity.SelectCityActivity;
 
 /**
  * Created by pc on 2016/11/29.
@@ -16,10 +18,18 @@ import com.demo.swt.mystudyappshop.R;
 
 public class CateGoryFragment extends Fragment {
     private Button city;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.category, container, false);
+        View view = inflater.inflate(R.layout.category, container, false);
+        view.findViewById(R.id.select_city).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), SelectCityActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 
