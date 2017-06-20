afd6ac547283f8b862edbc46c0e8f479015ec87f
sunwentao <sunwentao@primedu.cn>
2017-06-20 10:03:54 +0800

    徽章

---
+++ mylib/src/main/java/com/xys/libzxing/zxing/utils/Utils.java
@@ -0,0 +1,13 @@
+package com.xys.libzxing.zxing.utils;
+
+/**
+ * 介绍：这里写介绍
+ * 作者：sweet
+ * 邮箱：sunwentao@priemdu.cn
+ * 时间: 2017/6/20
+ */
+public class Utils {
+    public static String getVersion() {
+        return "1.0.0";
+    }
+}
