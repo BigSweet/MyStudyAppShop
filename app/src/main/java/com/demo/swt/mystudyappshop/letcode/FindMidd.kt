b78445a68323585ec07393ddff1950d1c9b4af44
sunwentao <wentao.sun@yintech.com>
2020-11-27 18:21:56 +0800

    add Z字变换

--- app/src/main/java/com/demo/swt/mystudyappshop/letcode/FindMidd.kt
+++ app/src/main/java/com/demo/swt/mystudyappshop/letcode/FindMidd.kt
@@ -11,9 +11,61 @@ package com.demo.swt.mystudyappshop.letcode
 object FindMidd {
     @JvmStatic
     fun main(args: Array<String>) {
-        print(findMedianSortedArrays(intArrayOf(1, 3, 5, 7, 9), intArrayOf(2, 4, 6, 8, 10)))
+//        print(findMedianSortedArrays(intArrayOf(1, 3, 5, 7, 9), intArrayOf(2, 4, 6, 8, 10)))
+        print(longestPalindrome("aba"))
     }
 
+    /**
+     * 最长回文？？
+     */
+    fun longestPalindrome(s: String): String? {
+        val n = s.length
+        val dp = Array(n) { BooleanArray(n) }
+        var ans = ""
+        for (l in 0 until n) {
+            var i = 0
+            while (i + l < n) {
+                val j = i + l
+                if (l == 0) {
+                    dp[i][j] = true
+                } else if (l == 1) {
+                    dp[i][j] = s[i] == s[j]
+                } else {
+                    dp[i][j] = s[i] == s[j] && dp[i + 1][j - 1]
+                }
+                if (dp[i][j] && l + 1 > ans.length) {
+                    ans = s.substring(i, i + l + 1)
+                }
+                ++i
+            }
+        }
+        return ans
+    }
+
+
+    /**
+     * Z字变换
+     */
+    fun convert(s: String, numRows: Int): String? {
+        if (numRows == 1) return s
+        val rows: MutableList<StringBuilder> = ArrayList()
+        for (i in 0 until numRows.coerceAtMost(s.length)) rows.add(StringBuilder())
+        var curRow = 0
+        var goingDown = false
+        for (c in s.toCharArray()) {
+            rows[curRow].append(c)
+            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown
+            curRow += if (goingDown) 1 else -1
+        }
+        val ret = StringBuilder()
+        for (row in rows) ret.append(row)
+        return ret.toString()
+    }
+
+
+    /**
+     * 寻找中位数
+     */
     fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
         if (nums1.size > nums2.size) {
             return findMedianSortedArrays(nums2, nums1)
